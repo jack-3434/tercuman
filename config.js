88986d49a2a6eb2fdd9d93bcf362a0b9734b1700
jack-3434 <easyboxmachine@gmail.com>
2026-06-13 21:34:59 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 // ====== AYAR ======
-// Google Apps Script "Web App" adresinizi buraya yapıştırın.
-// (Kurulum: README.md → Adım 2). Tırnakların içini değiştirin:
-const API_URL = "https://script.google.com/macros/s/AKfycbyATETC5jACJSZumLhLvktiPlHInvx_b1muvoXsXNzEhzLdz5GE-dGav1Ut9RxGEML2ig/exec";
+// Google Apps Script "Web App" adresiniz:
+window.API_URL = "https://script.google.com/macros/s/AKfycbyATETC5jACJSZumLhLvktiPlHInvx_b1muvoXsXNzEhzLdz5GE-dGav1Ut9RxGEML2ig/exec";
