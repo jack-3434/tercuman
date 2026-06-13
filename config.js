81a7a0d05ced2c854df389bd3e2a1b2cdf7c5ae7
jack-3434 <easyboxmachine@gmail.com>
2026-06-13 21:25:28 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // ====== AYAR ======
 // Google Apps Script "Web App" adresinizi buraya yapıştırın.
 // (Kurulum: README.md → Adım 2). Tırnakların içini değiştirin:
-const API_URL = "BURAYA_APPS_SCRIPT_URL_YAPISTIRIN";
+const API_URL = "https://script.google.com/macros/s/AKfycbyATETC5jACJSZumLhLvktiPlHInvx_b1muvoXsXNzEhzLdz5GE-dGav1Ut9RxGEML2ig/exec";
